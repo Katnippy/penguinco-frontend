9c1a23134815edc1e054a16b943c5103a6463e04
Katnippy <katnippy@proton.me>
2024-08-28 13:04:34 +0100

    Style error page.

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,13 +1,40 @@
+import Grid from '@mui/material/Grid';
+import { Box } from '@mui/material';
+import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom';
 
-export default function Error() {
+export default function NotFound() {
   return(
-    <div>
-      <h1>Error</h1>
-      <p>
-        An error has occurred! Click <Link to={'/stores'}>here</Link> to return
-        home.
-      </p>
-    </div>
+    <>
+      <style>
+        {
+          `html, body { 
+            background: url("../../images/confused.png") center center repeat; 
+            background-size: 10em; 
+            overflow-y: hidden;
+          }`
+        }
+      </style>
+      <Grid container justifyContent="center" alignItems="center"
+        minHeight="100vh">
+        <Grid item>
+          {/* TODO: Make this solution for lining up the tables & boxes on
+              TODO: mobile less hacky. */}
+          <Box id="box" border={1} borderRadius="5px" sx={{
+            width: { xs: '77vw', sm: '60vw', md: '50vw', lg: '30vw' }, p: 4
+          }}>
+            <Typography textAlign="center" gutterBottom
+              sx={{ typography: { xs: 'h4', sm: 'h3' } }}>
+              Error
+            </Typography>
+            <Typography textAlign="center"
+              sx={{ typography: { xs: 'h6', sm: 'h5' } }}>
+              An error has occurred! Click <Link to={'/stores'}>here</Link> to
+              return home.
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
+    </>
   );
 }
