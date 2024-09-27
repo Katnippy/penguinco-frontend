d5645d014435f28c00b9bd760563ab9fa721532a
Katnippy <katnippy@proton.me>
2024-09-27 13:51:59 +0100

    Remove leading zeroes from quantity input.

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -10,11 +10,17 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
 import { useAppDispatch, useAppSelector } from '../app/hooks';
-import { resetState, getStore, updateStore } from
-  '../features/store/storeSlice';
+import {
+  resetState,
+  getStore,
+  updateStore,
+} from '../features/store/storeSlice';
 import { IStockItem, IStock, IStore } from '../common/types';
-import { MAX_STOCK_QUANTITY, MIN_STOCK_QUANTITY, STOCK_ITEMS }
-  from '../common/consts';
+import {
+  MAX_STOCK_QUANTITY,
+  MIN_STOCK_QUANTITY,
+  STOCK_ITEMS,
+} from '../common/consts';
 import Notification from '../components/Notification';
 import NotFound from './NotFound';
 import Error from './Error';
@@ -36,27 +42,34 @@ export default function Store() {
   // filtering out existing item ids.
   let availableStock: Array<IStockItem> = [];
   if (typeof store !== 'undefined' && Object.keys(store).length) {
-    availableStock = STOCK_ITEMS!.filter((stockItem) =>
-      !store.stock.map((item) => item.stockItemId).includes(+stockItem.id)
+    availableStock = STOCK_ITEMS!.filter(
+      (stockItem) =>
+        !store.stock.map((item) => item.stockItemId).includes(+stockItem.id),
     );
   }
 
   // ? Shouldn't these be undefined?
-  const [newStock, setNewStock] =
-    useState<IStock>({ id: '1', stockItemId: 1, quantity: 0 });
+  const [newStock, setNewStock] = useState<IStock>({
+    id: '1',
+    stockItemId: 1,
+    quantity: 0,
+  });
 
   // ? Does this need to be a `useEffect()`?
   useEffect(() => {
     setNewStock({
-      id: typeof store !== 'undefined' && Object.keys(store).length &&
-        store.stock.length ? (+store.stock.at(-1)!.id + 1).toString() : '1',
+      id:
+        typeof store !== 'undefined' &&
+        Object.keys(store).length &&
+        store.stock.length
+          ? (+store.stock.at(-1)!.id + 1).toString()
+          : '1',
       stockItemId: !availableStock.length ? undefined : +availableStock[0].id,
       quantity: 0,
     });
   }, [store]);
 
-  function updateThenReadStore(storeToUpdate: IStore)
-  {
+  function updateThenReadStore(storeToUpdate: IStore) {
     dispatch(updateStore(storeToUpdate))
       // ? Should this promise be resolved, caught, etc.?
       .then(() => dispatch(getStore(+params.id!)));
@@ -65,9 +78,12 @@ export default function Store() {
   function incrementStock(itemId: string) {
     const storeToUpdate: IStore = {
       ...(store as IStore),
-      stock: [...store!.stock.map((item) => item.id === itemId ?
-        { ...item, quantity: item.quantity + 1 } : item)],
-      updated: DateTime.now().toISODate()
+      stock: [
+        ...store!.stock.map((item) =>
+          item.id === itemId ? { ...item, quantity: item.quantity + 1 } : item,
+        ),
+      ],
+      updated: DateTime.now().toISODate(),
     };
     updateThenReadStore(storeToUpdate);
   }
@@ -75,9 +91,12 @@ export default function Store() {
   function decrementStock(itemId: string) {
     const storeToUpdate: IStore = {
       ...(store as IStore),
-      stock: [...store!.stock.map((item) => item.id === itemId ?
-        { ...item, quantity: item.quantity - 1 } : item)],
-      updated: DateTime.now().toISODate()
+      stock: [
+        ...store!.stock.map((item) =>
+          item.id === itemId ? { ...item, quantity: item.quantity - 1 } : item,
+        ),
+      ],
+      updated: DateTime.now().toISODate(),
     };
     updateThenReadStore(storeToUpdate);
   }
@@ -86,7 +105,7 @@ export default function Store() {
     const storeToUpdate: IStore = {
       ...(store as IStore),
       stock: [...store!.stock.filter((item) => item.id !== itemId)],
-      updated: DateTime.now().toISODate()
+      updated: DateTime.now().toISODate(),
     };
     updateThenReadStore(storeToUpdate);
   }
@@ -97,7 +116,7 @@ export default function Store() {
     const storeToUpdate: IStore = {
       ...(store as IStore),
       stock: [...store!.stock, newStock],
-      updated: DateTime.now().toISODate()
+      updated: DateTime.now().toISODate(),
     };
     updateThenReadStore(storeToUpdate);
   }
@@ -112,43 +131,66 @@ export default function Store() {
 
   return (
     <>
-      {!loading && error &&
-        (error === '404' ? <NotFound /> : error === 'Error' ? <Error /> : '')
-      }
+      {!loading &&
+        error &&
+        (error === '404' ? <NotFound /> : error === 'Error' ? <Error /> : '')}
       {/* ! The error page is nondescript - needs a custom page! */}
-      {typeof store === 'undefined' ? <Error/> : ''}
+      {typeof store === 'undefined' ? <Error /> : ''}
       {typeof store !== 'undefined' && Object.keys(store).length ? (
         // Container
         <div id="container">
           {/* Content */}
-          <Grid container spacing={0} direction="column"
-            justifyContent="center" alignItems="center"
+          <Grid
+            container
+            spacing={0}
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
             sx={{ mx: { xs: 1, sm: 0 }, mt: { xs: 2, sm: 6, md: 0 } }}
           >
             <Grid item>
               {/* Title and return button */}
-              <Grid container spacing={2} maxWidth="100vw"
+              <Grid
+                container
+                spacing={2}
+                maxWidth="100vw"
                 sx={{ alignItems: { xs: 'center', sm: 'normal' }, mb: 1 }}
               >
                 {/* Title */}
-                <Grid item xs={6} sm={10} md={11}
-                  sx={{ paddingTop: { md: '0 !important' } }}>
-                  <Typography gutterBottom
-                    sx={{ typography: { xs: 'h4', md: 'h3' }, ml: 1 }}>
+                <Grid
+                  item
+                  xs={6}
+                  sm={10}
+                  md={11}
+                  sx={{ paddingTop: { md: '0 !important' } }}
+                >
+                  <Typography
+                    gutterBottom
+                    sx={{ typography: { xs: 'h4', md: 'h3' }, ml: 1 }}
+                  >
                     Manage {store.name}
                   </Typography>
                 </Grid>
                 {/* Return button */}
                 <Grid item xs={6} sm={2} md={1}>
-                  <Grid container justifyContent='center'
-                    sx={{ width: { xs: '83.5vw', sm: 'auto' } }}>
+                  <Grid
+                    container
+                    justifyContent="center"
+                    sx={{ width: { xs: '83.5vw', sm: 'auto' } }}
+                  >
                     <Grid item sx={{ ml: 1 }}>
                       <Link to={'/stores'}>
-                        <Button id="return" variant="outlined" sx={{
-                          mt: { md: -0.5 }, mr: { xs: 14, sm: 0, md: 9 },
-                          ml: { sm: -1, md: 1, lg: 3 }, mb: { xs: 2, sm: 0 }
-                        }}>
-                        Return
+                        <Button
+                          id="return"
+                          variant="outlined"
+                          sx={{
+                            mt: { md: -0.5 },
+                            mr: { xs: 14, sm: 0, md: 9 },
+                            ml: { sm: -1, md: 1, lg: 3 },
+                            mb: { xs: 2, sm: 0 },
+                          }}
+                        >
+                          Return
                         </Button>
                       </Link>
                     </Grid>
@@ -156,71 +198,119 @@ export default function Store() {
                 </Grid>
               </Grid>
               <Notification />
-              <StoreTable loading={loading} stock={store.stock}
-                incrementStock={incrementStock} decrementStock={decrementStock}
+              <StoreTable
+                loading={loading}
+                stock={store.stock}
+                incrementStock={incrementStock}
+                decrementStock={decrementStock}
                 deleteStock={deleteStock}
               />
               {/* New stock box */}
-              <Grid container flexDirection="column" maxWidth="100vw"
+              <Grid
+                container
+                flexDirection="column"
+                maxWidth="100vw"
                 sx={{ justifyContent: 'center', alignItems: 'center' }}
               >
                 {/* Title */}
                 <Grid item>
-                  <Typography variant="h5" textAlign="center" gutterBottom
-                    sx={{ mt: 3, mr: { xs: 4, sm: 0 } }}>
-                  New stock
+                  <Typography
+                    variant="h5"
+                    textAlign="center"
+                    gutterBottom
+                    sx={{ mt: 3, mr: { xs: 4, sm: 0 } }}
+                  >
+                    New stock
                   </Typography>
                 </Grid>
                 {/* New stock form */}
                 <Grid item>
-                  <Box id="box" border={1} borderRadius="5px" sx={{
-                    width: { xs: '90vw', sm: '60vw', md: '50vw', lg: '30vw' },
-                    mr: { xs: 4, sm: 0 }
-                  }}>
+                  <Box
+                    id="box"
+                    border={1}
+                    borderRadius="5px"
+                    sx={{
+                      width: { xs: '90vw', sm: '60vw', md: '50vw', lg: '30vw' },
+                      mr: { xs: 4, sm: 0 },
+                    }}
+                  >
                     <form onSubmit={addStock}>
                       <FormControl fullWidth>
-                        <Grid container sx={{ spacing: 2,
-                          my: { xs: 3, sm: 3.5 } }}>
+                        <Grid
+                          container
+                          sx={{ spacing: 2, my: { xs: 3, sm: 3.5 } }}
+                        >
                           {/* Name input */}
                           <Grid item xs={6} sm={5}>
-                            <Grid container sx={{ display: 'flex',
-                              justifyContent: 'center' }}>
-                              <TextField select name="name" id="name"
-                                value={newStock.stockItemId === undefined ? ''
-                                  : newStock.stockItemId}
-                                label="Name" onChange={handleNewStockName}
+                            <Grid
+                              container
+                              sx={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                              }}
+                            >
+                              <TextField
+                                select
+                                name="name"
+                                id="name"
+                                value={
+                                  newStock.stockItemId === undefined
+                                    ? ''
+                                    : newStock.stockItemId
+                                }
+                                label="Name"
+                                onChange={handleNewStockName}
                               >
-                                {availableStock.map((stock) =>
+                                {availableStock.map((stock) => (
                                   <MenuItem key={stock.id} value={stock.id}>
                                     {stock.name}
                                   </MenuItem>
-                                )}
+                                ))}
                               </TextField>
                             </Grid>
                           </Grid>
                           {/* Quantity input */}
                           <Grid item xs={6} sm={3}>
-                            <Grid container sx={{ display: 'flex',
-                              justifyContent: 'center' }}>
+                            <Grid
+                              container
+                              sx={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                              }}
+                            >
                               <TextField
-                                type="number" id="quantity" label="Quantity"
-                                value={newStock.quantity}
+                                type="number"
+                                id="quantity"
+                                label="Quantity"
+                                value={newStock.quantity.toString()}
                                 onChange={handleNewStockQuantity}
-                                InputProps= {{ inputProps: {
-                                  min: MIN_STOCK_QUANTITY,
-                                  max: MAX_STOCK_QUANTITY
-                                } }}
+                                InputProps={{
+                                  inputProps: {
+                                    min: MIN_STOCK_QUANTITY,
+                                    max: MAX_STOCK_QUANTITY,
+                                  },
+                                }}
                               />
                             </Grid>
                           </Grid>
                           {/* Submit button */}
-                          <Grid item xs={12} sm={4}
-                            sx={{ display: 'flex' , justifyContent: 'center',
-                              mt: { xs: 2, sm: 1.25 }, mb: { xs: -1, sm: 0 } }}
+                          <Grid
+                            item
+                            xs={12}
+                            sm={4}
+                            sx={{
+                              display: 'flex',
+                              justifyContent: 'center',
+                              mt: { xs: 2, sm: 1.25 },
+                              mb: { xs: -1, sm: 0 },
+                            }}
                           >
-                            <Button type="submit" variant="contained"
-                              sx={{ alignSelf: 'flex-start' }}>
-                            Submit
+                            <Button
+                              type="submit"
+                              variant="contained"
+                              sx={{ alignSelf: 'flex-start' }}
+                            >
+                              Submit
                             </Button>
                           </Grid>
                         </Grid>
@@ -232,7 +322,9 @@ export default function Store() {
             </Grid>
           </Grid>
         </div>
-      ) : ''}
+      ) : (
+        ''
+      )}
     </>
   );
 }
